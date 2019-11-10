e9cb9f94234fd5819fe7cfa4a6171792d9c44dfe
srivastava9 <krishu4626@gmail.com>
2019-11-10 16:56:19 +0530

    Updates

---
+++ precache-manifest.e6b36c1bde5fc51861a5622d0213cb87.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d29f20fda39824f681dea932a6486c9",
+    "url": "/Confluencia/index.html"
+  },
+  {
+    "revision": "6a5e556d5ae240f90446",
+    "url": "/Confluencia/static/css/main.c5df2e35.chunk.css"
+  },
+  {
+    "revision": "75747f53373d18caf212",
+    "url": "/Confluencia/static/js/2.9fe8590e.chunk.js"
+  },
+  {
+    "revision": "6a5e556d5ae240f90446",
+    "url": "/Confluencia/static/js/main.e7b7d082.chunk.js"
+  },
+  {
+    "revision": "427b8acc2281ff2ec44c",
+    "url": "/Confluencia/static/js/runtime-main.0e46f8b9.js"
+  },
+  {
+    "revision": "1d4109866e2f97948a48601b910c73db",
+    "url": "/Confluencia/static/media/Mukesh_Jain.1d410986.png"
+  },
+  {
+    "revision": "798767c5b903af3b9d5e0b8ff2ab08b5",
+    "url": "/Confluencia/static/media/at.798767c5.png"
+  },
+  {
+    "revision": "c33b1f1e61444814e05acae6fce9cc16",
+    "url": "/Confluencia/static/media/events_bhumika.c33b1f1e.jpeg"
+  },
+  {
+    "revision": "bc1273f6c133f24331ce8bb20c949e9e",
+    "url": "/Confluencia/static/media/hod.bc1273f6.png"
+  },
+  {
+    "revision": "8e16048c2fcfaba9c0e6e3579aeb6811",
+    "url": "/Confluencia/static/media/homepage_1_c.8e16048c.jpg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/Confluencia/static/media/logo.d41d8cd9.svg"
+  }
+]);
