f779bff839df83930359dbff1f4c8d35fa688bee
srivastava9 <krishu4626@gmail.com>
2020-01-16 02:46:33 +0530

    changes

--- frontend/confluencia/src/App.js
+++ frontend/confluencia/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import logo from "./logo.svg";
+import { BrowserRouter, Route } from "react-router-dom";
+
 import "./App.css";
 import Button_homepage from "./components/button/button_1.js";
 import Title_homepage from "./components/title_homepage/title_homepage.js";
 import Homepage from "./homepage/homepage.js";
+import Registration from "./components/registration";
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/" component={Homepage} />
+        <Route path="/register" component={Registration} />
+      </div>
+    </BrowserRouter>
   );
 }
 
