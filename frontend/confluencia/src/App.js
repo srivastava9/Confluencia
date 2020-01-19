0fe764300999643a224bf1aa1dfcfffc69bef759
formica <manuseth30@gmail.com>
2020-01-20 03:23:26 +0530

    event page added

--- frontend/confluencia/src/App.js
+++ frontend/confluencia/src/App.js
@@ -3,11 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import Button_homepage from "./components/button/button_1.js";
 import Title_homepage from "./components/title_homepage/title_homepage.js";
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import Homepage from "./homepage/homepage.js";
+import Abhivyakti from "../src/components/Eventsall/abhivyakti";
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Router>
+        <div>
+          <Route exact path="/" component={Homepage}/>
+          <Route path="/event" component={Abhivyakti}/>
+        </div>
+      </Router>
     </div>
   );
 }
