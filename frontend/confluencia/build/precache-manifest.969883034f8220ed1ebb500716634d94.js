6d9da06bc42b734cf224dda96ba3af29172afde3
srivastava9 <krishu4626@gmail.com>
2020-02-02 00:56:00 +0530

    [feat]: Build pushed

---
+++ frontend/confluencia/build/precache-manifest.969883034f8220ed1ebb500716634d94.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce3e6e2e5c864f2475b9116b9df37d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b47295bd4e85401bc09d",
+    "url": "/static/css/main.f28faa85.chunk.css"
+  },
+  {
+    "revision": "277a7a0c62bcaea8a4bc",
+    "url": "/static/js/2.2ec75932.chunk.js"
+  },
+  {
+    "revision": "b47295bd4e85401bc09d",
+    "url": "/static/js/main.9bc919aa.chunk.js"
+  },
+  {
+    "revision": "6c9873f070369031feb3",
+    "url": "/static/js/runtime-main.b1df109b.js"
+  },
+  {
+    "revision": "0a01892e588a2a69348d786621f2cb0a",
+    "url": "/static/media/DOMS_logo.0a01892e.png"
+  },
+  {
+    "revision": "54a4f9c6772be425461eafddd274ea46",
+    "url": "/static/media/DataWars.54a4f9c6.png"
+  },
+  {
+    "revision": "ea7923bbf6774fbfc5ea52dad4eca5a9",
+    "url": "/static/media/Environmentalist.ea7923bb.png"
+  },
+  {
+    "revision": "1d4109866e2f97948a48601b910c73db",
+    "url": "/static/media/Mukesh_Jain.1d410986.png"
+  },
+  {
+    "revision": "ecacd4f3ada6f82be5ab32a4b6911c8e",
+    "url": "/static/media/abhivyakti_image.ecacd4f3.png"
+  },
+  {
+    "revision": "798767c5b903af3b9d5e0b8ff2ab08b5",
+    "url": "/static/media/at.798767c5.png"
+  },
+  {
+    "revision": "bc1273f6c133f24331ce8bb20c949e9e",
+    "url": "/static/media/hod.bc1273f6.png"
+  },
+  {
+    "revision": "8e16048c2fcfaba9c0e6e3579aeb6811",
+    "url": "/static/media/homepage_1_c.8e16048c.jpg"
+  },
+  {
+    "revision": "e9690720f0cbaf9e68f0adc27ad08c36",
+    "url": "/static/media/image_1.e9690720.png"
+  },
+  {
+    "revision": "f44cb623a52e3457bd3575f92c5a5641",
+    "url": "/static/media/image_2.f44cb623.png"
+  },
+  {
+    "revision": "92dd6975b49a1f3a8e3593258fe2ba66",
+    "url": "/static/media/image_4.92dd6975.png"
+  },
+  {
+    "revision": "4a08a6894c2306aa8dd08baeccd32116",
+    "url": "/static/media/image_5.4a08a689.png"
+  },
+  {
+    "revision": "408a3f13c9753faef2c67a8716ac98c9",
+    "url": "/static/media/image_6.408a3f13.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/logo.d41d8cd9.svg"
+  },
+  {
+    "revision": "fd39a11681d8fd2ee45090db98d27300",
+    "url": "/static/media/logo.fd39a116.png"
+  },
+  {
+    "revision": "2934c3a7a00efe1510764426ac30a97e",
+    "url": "/static/media/outofthebox.2934c3a7.jpg"
+  },
+  {
+    "revision": "a9e468d1e162349d45bae73f295907eb",
+    "url": "/static/media/professor1.a9e468d1.png"
+  },
+  {
+    "revision": "3b5a77fca7daf75a6a765abfa73d8343",
+    "url": "/static/media/professor2.3b5a77fc.png"
+  },
+  {
+    "revision": "7501f8c71ce6d38a9c579d561824494e",
+    "url": "/static/media/professor3.7501f8c7.png"
+  },
+  {
+    "revision": "93b744c20f90259b616dcd4fce1f4c0a",
+    "url": "/static/media/professor4.93b744c2.png"
+  },
+  {
+    "revision": "460e9dd88f37e3b501c2b2a2fc2f3b3f",
+    "url": "/static/media/revolution_image.460e9dd8.jpg"
+  }
+]);
